988e9a2c8fd25cded300aa1ee87873a36ee59dcf
Colm Murphy <65133392+ColmMurphy233@users.noreply.github.com>
2021-01-03 11:58:52 +0000

    Delete Secrets.kt

--- src/main/java/xyz/colmmurphy/d2mc/Enums/Secrets.kt
+++
@@ -1,7 +0,0 @@
-package xyz.colmmurphy.d2mc.Enums
-
-enum class Secrets(val id: String) {
-    GUILD("750706717796466779"),
-    CHANNEL("792705933335461928"),
-    BOT_TOKEN("NzkyNzA0NjYwMTUwMDI2MjUw.X-hlvg.aFZpgOTtX4EYIlAIgdeSWZD7qRY");
-}
