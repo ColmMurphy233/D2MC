6e612b407bd8006d9c6e99e765d034180d8a3011
colm <colmmurphy016@gmail.com>
2021-01-03 11:25:38 +0000

    v1.1.0 release

--- src/main/java/xyz/colmmurphy/d2mc/Enums/Secrets.kt
+++ src/main/java/xyz/colmmurphy/d2mc/Enums/Secrets.kt
@@ -3,5 +3,5 @@ package xyz.colmmurphy.d2mc.Enums
 enum class Secrets(val id: String) {
     GUILD("750706717796466779"),
     CHANNEL("792705933335461928"),
-    BOT_TOKEN("NzkyNzA0NjYwMTUwMDI2MjUw.X-hlvg.dwn_LGeQxLlTD8Xy5ed4sKfpdBA");
+    BOT_TOKEN("NzkyNzA0NjYwMTUwMDI2MjUw.X-hlvg.aFZpgOTtX4EYIlAIgdeSWZD7qRY");
 }
