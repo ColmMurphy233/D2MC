d295942cbaf684619bb37c8b49d9958fccae58d8
colm <colmmurphy016@gmail.com>
2021-01-09 12:03:37 +0000

    v1.2.3

--- src/main/java/xyz/colmmurphy/d2mc/Listeners/PlayerDeathListener.kt
+++ src/main/java/xyz/colmmurphy/d2mc/Listeners/PlayerDeathListener.kt
@@ -1,5 +1,8 @@
 package xyz.colmmurphy.d2mc.Listeners
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
@@ -8,7 +11,10 @@ import xyz.colmmurphy.d2mc.Inbox.Inbox
 class PlayerDeathListener : Listener {
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
-        Inbox.post("**${event.deathMessage}**")
-        Inbox.reactMostRecentMsg("U+1F1EB")
+        GlobalScope.launch {
+            Inbox.post("**${event.deathMessage}**")
+            delay(1000)
+            Inbox.reactMostRecentMsg("U+1F1EB")
+        }
     }
 }
